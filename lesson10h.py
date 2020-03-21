fd3b914d482e7cb55c73218791aaedf1345516e2
Boris Gelfandbein <boris@lt.com.ua>
2020-03-21 20:06:30 +0200

    1

--- lesson10h.py
+++ lesson10h.py
@@ -1,6 +1,8 @@
-# Lesson 10: Classes, scope, and namespaces
+""" Lesson 10: Classes, scope, and namespaces """
 
-class Person():
+import sys
+
+class Person(firstname,lastname,age):
     def __init__(self,firstname,lastname,age):
         self.firstname = firstname
         self.lastname = lastname
@@ -8,3 +10,13 @@ class Person():
 
     def talk():
         print(f“Hello, my name is {firstname} {lastname} and I’m {age} years old”)
+
+def main():
+    p = set(input("Enter your Firstname, Lastname & age separated by comas: "))
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nGot KeyboardInterrupt! Exiting...")
+        sys.exit()
