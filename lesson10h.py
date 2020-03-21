9924e2fea332a06cb204a8e2201238f071717762
Boris Gelfandbein <boris@lt.com.ua>
2020-03-21 19:51:01 +0200

    2

---
+++ lesson10h.py
@@ -0,0 +1,10 @@
+# Lesson 10: Classes, scope, and namespaces
+
+class Person():
+    def __init__(self,firstname,lastname,age):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+
+    def talk():
+        print(f“Hello, my name is {firstname} {lastname} and I’m {age} years old”)
