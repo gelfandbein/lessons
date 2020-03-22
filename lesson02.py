a034ebd031c93d151110361051e95527ea8e54c6
Boris Gelfandbein <boris@lt.com.ua>
2020-03-22 10:22:44 +0200

    PC-2019.3.3 <boris@MacBook-Boris.local Overwrite remote https://github.com/gelfandbein/lessons to local

--- lesson02.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 26 15:27:17 2020
-
-@author: boris
-"""
-
-print("we have run lesson02")
--- lesson03.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Feb 29 12:36:48 2020
-
-@author: boris
-"""
-
