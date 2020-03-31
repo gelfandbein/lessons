ee779c726770cb994485d4c4cdd554ee74102c31
Boris Gelfandbein <boris@lt.com.ua>
2020-04-01 00:07:13 +0300

    next

--- 17/test_tvcontroller.py
+++ 17/test_tvcontroller.py
@@ -1,50 +1,61 @@
 import unittest
-from calculator import Calculator
+from unittest.mock import patch
+import mock
+import io
+import sys
+
+from tvcontroller import TVController
 
 """
 Should be userd with coverage
-coverage run --omit=test_calculator.py -m unittest test_calculator
+coverage run --omit=test_tvcontroller.py -m unittest test_tvcontroller
 """
 
 class TestTVController(unittest.TestCase):
     def setUp(self):
-        self.calc = Calculator()
+        self.tvctl = TVController()
+        self.debug = True
 
-    def test_calc_name(self):
-        calc = Calculator("My Calculator")
-        self.assertEqual(calc.name, "My Calculator")
+    def tearDown(self):
+        pass
 
-    def test_add(self):
-        cases = [
-            {"in": (2, 2), "out": 4},
-            {"in": (2, 3), "out": 5},
-        ]
+    def test_init(self):
+        self.assertEqual(self.tvctl.name, "My TVController")
+        self.assertEqual(self.tvctl.list, ["BBC", "Discovery", "TV1000"])
+        self.assertEqual(self.tvctl.status, 0)
+        self.assertEqual(self.tvctl.channel, 0)
 
-        for case in cases:
-            self.assertEqual(self.calc.add(case["in"][0], case["in"][1]), case["out"])
-        # self.assertEqual(self.calc.add(2, 2), 4)
-        # self.assertNotEqual(self.calc.add(3, 3), 7)
+    #@unittest.skip("skipping right now")
+    # def test_test(self):
+    #     self.assertEqual(self.tvctl.status, 0)
+    #     self.assertFalse(self.tvctl.status == None)
+    #     with mock.patch('builtins.input', return_value="y"):
+    #         assert self.tvctl.test() != "Hardware test passed!"
 
-    def test_minus(self):
-        self.assertEqual(self.calc.minus(3, 3), 0)
-        self.assertNotEqual(self.calc.minus(2, 2), 1)
+    @patch('builtins.input', return_value='y') # side_effect=["y", "yes"])
+    def test_switch(self, value):
+        channel = self.tvctl.channel
+        saved_stdout = sys.stdout
+        try:
+            out = io.StringIO()
+            sys.stdout = out
+            #self.switch(channel)
+            output = out.getvalue().strip()
+            self.assertEqual(output, "Switching...")
+        finally:
+            sys.stdout = saved_stdout
 
-    def test_sum(self):
-        self.assertEqual(self.calc.sum(3, 3, 4), 10)
-        self.assertNotEqual(self.calc.sum(3, 3, 4), 11)
 
-    def test_div(self):
-        res = self.calc.div(6, 3)
-        self.assertEqual(res, 2, "6 / 3 should be 2")
+    # def test_switch(self):
+    #     self.assertFalse(self.tvctl.channel > len(self.tvctl.list))
 
-    def test_div_error(self):
-        with self.assertRaises(ValueError):
-            self.calc.div(6, 0)
-            # res = self.calc.div(6, 0)
-            # self.assertEqual(res, None, "Division by zero")
+    def test_main(self):
+        pass
+        #self.assertEqual('p', 'n', "Oops! Sorry!")
 
 
 
 if __name__ == "__main__":
-    unittest.main()
-
+    #unittest.main()
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestTVController)
+    unittest.TextTestRunner(verbosity=2).run(suite)
--- 17/tvcontroller.py
+++ 17/tvcontroller.py
@@ -1,40 +1,46 @@
-# Lesson 17: Test cases
-
+##
+# Lesson 17: Test test cases
+##
 import sys
 
-class TVController():
+class TVController:
     def __init__(self):
+        self.name = "My TVController"
         self.list = ["BBC", "Discovery", "TV1000"]
         self.status = 0
         self.channel = 0
 
     def test(self):
         if self.status == 0:
-            status = input("Yours TV is turned 0FF. Turn it 0N? (y/n): ")
-            if status == "y":
+            ss = input("Yours TV is turned 0FF. Turn it 0N? (y/n): ")
+            if ss == "y" or "yes":
+                self.status = 1
                 return print("Hardware test passed!")
             else:
+                self.status = 0
                 raise KeyboardInterrupt
 
 
-    def swith(self, channel):
+    def switch(self, channel):
         if channel == len(self.list):
             self.channel = 0
         elif channel < 0:
             self.channel = len(self.list)-1
         
         print(f"-> '{self.list[self.channel]}' channel found. Switching...")
-        controller.main()
+        return controller.main()
 
     def main(self):
+        controller.test()
+
         print(f"Now you watch the {self.list[self.channel]} channel. You have {self.list} channels.")
         choice = input("Do you want switch the channel? (p)revious or (n)ext (q - to quit): ")
         if choice == "n":
             self.channel = self.channel + 1
-            controller.swith(self.channel)
+            controller.switch(self.channel)
         elif choice == "p":
             self.channel = self.channel - 1
-            controller.swith(self.channel)
+            controller.switch(self.channel)
         else:
             raise KeyboardInterrupt
 
@@ -42,7 +48,6 @@ class TVController():
 if __name__ == "__main__":
     controller = TVController()
     try:
-        controller.test()
         controller.main()
     except KeyboardInterrupt:
         print("\nGot STOP. Turning 0FF & exiting from this fucking world...")
